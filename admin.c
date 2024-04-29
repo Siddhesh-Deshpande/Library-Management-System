857903aa561f5c356bafdbe19a5a9abc39b35a7c
Siddhesh Deshpande <Siddhesh.Deshpande@iiitb.ac.in>
2024-04-30 00:24:39 +0530

    File Locking implemented

--- admin.c
+++ admin.c
@@ -8,26 +8,62 @@ void add_log(char *information)
     fprintf(ptr,"%s",information);
     fclose(ptr);
 }
+bool perform_unlock(int fd, struct flock *wrlk)
+{
+    wrlk->l_type=F_UNLCK;
+    int status = fcntl(fd,F_SETLKW,wrlk);
+    if(status==-1)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
+bool perform_wlock(int fd , struct flock *wrlk)
+{
+    wrlk->l_type=F_WRLCK;
+    wrlk->l_whence=SEEK_SET;
+    wrlk->l_start=0;
+    wrlk->l_len=0;
+    wrlk->l_pid=getpid();
+    int status = fcntl(fd,F_SETLKW,wrlk);
+    if(status==-1)
+    {
+        return false;
+    }
+    else
+    {
+        return true;
+    }
+}
 bool add_user(Person p)
 {
     int fd = open("user_details.txt",O_RDWR|O_CREAT,0666);
     if(fd==-1)
     {
-        // printf("User add failed\n");
         add_log("User add failed\n");
         return false;
     }
+    struct flock wrlk; // Performing file locking.
+    perform_wlock(fd,&wrlk);
+    //remove thiss
+    // printf("Sleeping for testing\n");;
+    // sleep(50);
+    // remove this.
     int st1 = lseek(fd,0,SEEK_END);
     int st2 = write(fd,&p,sizeof(p));
     if(st1==-1 || st2==-1)
     {
-        // printf("User ADD Failed\n");
+        perform_unlock(fd,&wrlk);
         add_log("User add failed\n");
         return false;
     }
     add_log("New User ");
     add_log(p.name);
     add_log("has been added \n");
+    perform_unlock(fd,&wrlk);//Unlocking the file.
     close(fd);
     return true;
 }
@@ -36,21 +72,27 @@ bool add_book(Book b)
     int fd = open("book_details.txt",O_RDWR|O_CREAT,0666);
     if(fd==-1)
     {
-        // printf("Book Add Failed\n");
         add_log("Book Add Failed\n");
         return false;
     }
+    struct flock wrlk;
+    perform_wlock(fd,&wrlk);
+    //remove thiss
+    // printf("Sleeping for testing\n");;
+    // sleep(50);
+    // remove this.
     int s1 = lseek(fd,0,SEEK_END);
     int s2= write(fd,&b,sizeof(b));
     if(s1==-1 || s2==-1)
     {
-        // printf("Book Add Failed\n");
+        perform_unlock(fd,&wrlk);
         add_log("Book Add Failed\n");
         return false;
     }
     add_log("New Book ");
     add_log(b.book_name);
     add_log("has been added\n");
+    perform_unlock(fd,&wrlk);
     close(fd);
     return true;
 }
@@ -79,15 +121,23 @@ void get_book(Book *b,int isbn)
     {
         if(temp.isbn==isbn)
         {
+            // read locking the specific record only.
+            struct flock rdlk;
+            rdlk.l_type=F_RDLCK;
+            rdlk.l_whence=lseek(fd,0,SEEK_CUR)-sizeof(temp);
+            rdlk.l_start=0;
+            rdlk.l_len=sizeof(temp);
+            rdlk.l_pid=getpid();
+            fcntl(fd,F_SETLKW,&rdlk);
             *b=temp;
             close(fd);
             add_log("Details of ");
             add_log(temp.book_name);
             add_log("were obtained successfully\n");
+            perform_unlock(fd,&rdlk);
             return;
         }
     }
-    // printf("Book Not found\n");
     add_log("Book ");
     add_log(b->book_name);
     add_log("not found\n");
@@ -111,9 +161,18 @@ bool modify_book(int isbn, int newval)
     {
         if(temp.isbn==isbn)
         {
+            //Write locking the specific record only.   
+            struct flock wrlk;
+            wrlk.l_type=F_WRLCK;
+            wrlk.l_whence=lseek(fd,0,SEEK_CUR)-sizeof(temp);
+            wrlk.l_start=0;
+            wrlk.l_len=sizeof(temp);
+            wrlk.l_pid=getpid();
+            fcntl(fd,F_SETLKW,&wrlk);
             temp.quantity=newval;
             lseek(fd,-sizeof(temp),SEEK_CUR);
             write(fd,&temp,sizeof(temp));
+            perform_unlock(fd,&wrlk);
             close(fd);
             add_log("The quantity of Book ");
             add_log(temp.book_name);
--- user.c
+++ user.c
@@ -15,9 +15,18 @@ bool issue_book(int isbn,char *person_name)
     {
         if(temp.isbn == isbn && temp.quantity>0)
         {
+            struct flock wrlk;
+            wrlk.l_type=F_WRLCK;
+            wrlk.l_whence=lseek(fd,0,SEEK_CUR)-sizeof(temp);
+            wrlk.l_start=0;
+            wrlk.l_len=sizeof(temp);
+            wrlk.l_pid=getpid();
+            fcntl(fd,F_SETLKW,&wrlk);
             temp.quantity--;
             lseek(fd,-sizeof(temp),SEEK_CUR);
             write(fd,&temp,sizeof(temp));
+            wrlk.l_type=F_UNLCK;
+            fcntl(fd,F_SETLK,&wrlk);
             close(fd);
             add_log(temp.book_name);
             add_log("has been issued to ");
@@ -45,9 +54,18 @@ bool return_book(int isbn)
     {
         if(temp.isbn==isbn)
         {
+            struct flock wrlk;
+            wrlk.l_type=F_WRLCK;
+            wrlk.l_whence=lseek(fd,0,SEEK_CUR)-sizeof(temp);
+            wrlk.l_start=0;
+            wrlk.l_len=sizeof(temp);
+            wrlk.l_pid=getpid();
+            fcntl(fd,F_SETLKW,&wrlk);
             temp.quantity++;
             lseek(fd,-sizeof(temp),SEEK_CUR);
             write(fd,&temp,sizeof(temp));
+            wrlk.l_type=F_UNLCK;
+            fcntl(fd,F_SETLK,&wrlk);
             close(fd);
             add_log(temp.book_name);
             add_log("was returned\n");
